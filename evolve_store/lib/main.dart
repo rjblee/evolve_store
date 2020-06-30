85d68794546c422381524c3b5a02cecfa106ba0a
Ralph Lee <ralph.lee@live.ca>
2020-06-29 19:33:58 -0700

    Basic setup with Cloud Firestore package

--- evolve_store/lib/main.dart
+++ evolve_store/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,6 +8,26 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Evolve Store"),
+      ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('evolve_items').snapshots(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            default:
+              return ListView(
+                children: [makeLi],
+              );
+          }
+          ;
+        },
+      ),
+    );
   }
 }
