ae3171fae8b73e7b5303612900b9ba80a5ff0cd5
Ralph Lee <ralph.lee@live.ca>
2020-07-02 15:19:34 -0700

    Refactor out SlideList widget

--- evolve_store/lib/main.dart
+++ evolve_store/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:evolvestore/widgets/slide_list.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -8,60 +9,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  List<Widget> makeProductList(AsyncSnapshot snapshot) {
-    return snapshot.data.documents.map<Widget>((document) {
-      return NeumorphicButton(
-        margin: EdgeInsets.symmetric(horizontal: 20),
-        child: Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: 10,
-          ),
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/backpack.png",
-                height: 150,
-              ),
-              SizedBox(height: 16),
-              Text(
-                document["name"],
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              Text(
-                "\$" + document["price"].toString(),
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Text(document["category"]),
-            ],
-          ),
-        ),
-        onPressed: () {},
-      );
-    }).toList();
-  }
-
   Widget buildProduct() {
-    return StreamBuilder(
-      stream: Firestore.instance.collection('products').snapshots(),
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          default:
-            return Neumorphic(
-              child: SizedBox(
-                height: 250,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: makeProductList(snapshot),
-                ),
-              ),
-            );
-        }
-      },
-    );
+    return SlideList();
   }
 
   @override
