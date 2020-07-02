16c210fd03b5115c4a88159034df42648f310cd5
Ralph Lee <ralph.lee@live.ca>
2020-07-02 14:50:16 -0700

    Implement Neumorphic package with product lists

--- evolve_store/lib/main.dart
+++ evolve_store/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,22 +10,37 @@ void main() {
 class MyApp extends StatelessWidget {
   List<Widget> makeProductList(AsyncSnapshot snapshot) {
     return snapshot.data.documents.map<Widget>((document) {
-      return Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20,
+      return NeumorphicButton(
+        margin: EdgeInsets.symmetric(horizontal: 20),
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 10,
+          ),
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/backpack.png",
+                height: 150,
+              ),
+              SizedBox(height: 16),
+              Text(
+                document["name"],
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "\$" + document["price"].toString(),
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(document["category"]),
+            ],
+          ),
         ),
-        child: Column(
-          children: [
-            Text(document["name"]),
-            Text(document["price"].toString()),
-            Text(document["category"]),
-          ],
-        ),
+        onPressed: () {},
       );
     }).toList();
   }
 
-  Widget buildProducts() {
+  Widget buildProduct() {
     return StreamBuilder(
       stream: Firestore.instance.collection('products').snapshots(),
       builder: (context, snapshot) {
@@ -33,11 +50,13 @@ class MyApp extends StatelessWidget {
               child: CircularProgressIndicator(),
             );
           default:
-            return SizedBox(
-              height: 150,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: makeProductList(snapshot),
+            return Neumorphic(
+              child: SizedBox(
+                height: 250,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: makeProductList(snapshot),
+                ),
               ),
             );
         }
@@ -47,15 +66,49 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Evolve Store"),
-        ),
-        body: Column(
-          children: [
-            buildProducts(),
-          ],
+    return NeumorphicApp(
+      theme: NeumorphicThemeData(
+//        baseColor: Color(0xFFffffff),
+        baseColor: Color(0xFFf4faff),
+
+        lightSource: LightSource.topLeft,
+        depth: 10,
+      ),
+      home: Neumorphic(
+        child: Scaffold(
+          appBar: NeumorphicAppBar(
+//            color: Colors.grey[300],
+            title: NeumorphicText(
+              "Evolve Store",
+//              textAlign: ,
+              style: NeumorphicStyle(
+                color: Colors.grey[900],
+                boxShape: NeumorphicBoxShape.circle(),
+              ),
+              textStyle: NeumorphicTextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Lobster',
+              ),
+            ),
+            leading: NeumorphicButton(
+              child: Icon(
+                Icons.menu,
+                color: Colors.grey[700],
+              ),
+              style: NeumorphicStyle(
+                depth: 5,
+                boxShape:
+                    NeumorphicBoxShape.roundRect(BorderRadius.circular(10)),
+              ),
+              onPressed: () {},
+            ),
+          ),
+          body: Column(
+            children: [
+              buildProduct(),
+            ],
+          ),
         ),
       ),
     );
