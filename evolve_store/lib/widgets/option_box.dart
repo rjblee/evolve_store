a3577ec2617f0558a6923964c57f718143e6f288
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-03 18:25:47 -0700

    Change Neumorphic widget to Container widget with BoxShadow code

--- evolve_store/lib/widgets/option_box.dart
+++ evolve_store/lib/widgets/option_box.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
@@ -28,6 +27,7 @@ class _OptionBoxState extends State<OptionBox> {
         ),
       );
     }
+
     return Column(
       children: [
         Padding(
@@ -35,17 +35,35 @@ class _OptionBoxState extends State<OptionBox> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("Choose the option:", style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16
-              ),),
-              Text("See all", style: TextStyle(
-                color: Colors.lightGreen,
-              ),)
+              Text(
+                "Choose the option:",
+                style: TextStyle(color: Colors.grey, fontSize: 16),
+              ),
+              Text(
+                "See all",
+                style: TextStyle(
+                  color: Colors.lightGreen,
+                ),
+              )
             ],
           ),
         ),
-        Neumorphic(
+        Container(
+          decoration: BoxDecoration(
+              color: Color(0XFFEFF3F6),
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.1),
+                    offset: Offset(6, 2),
+                    blurRadius: 6.0,
+                    spreadRadius: 3.0),
+                BoxShadow(
+                    color: Color.fromRGBO(255, 255, 255, 0.9),
+                    offset: Offset(-6, -2),
+                    blurRadius: 6.0,
+                    spreadRadius: 3.0)
+              ]),
           padding: EdgeInsets.all(15),
           child: Row(
             children: [
@@ -57,13 +75,13 @@ class _OptionBoxState extends State<OptionBox> {
               OptionBoxDetail(color: Colors.greenAccent),
             ],
           ),
-          style: NeumorphicStyle(
-            shape: NeumorphicShape.concave,
-            surfaceIntensity: 0.2,
-            intensity: 0.7,
-            depth: 4,
-            color: Color(0xffeef4fb),
-          ),
+//          style: NeumorphicStyle(
+//            shape: NeumorphicShape.concave,
+//            surfaceIntensity: 0.2,
+//            intensity: 0.7,
+//            depth: 4,
+//            color: Color(0xffeef4fb),
+//          ),
         )
       ],
     );
