0144c9c6a052d5831fe597ec0e04f25db90715dd
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-03 17:47:15 -0700

    Modified Product image's size based on device size

--- evolve_store/lib/widgets/circle_product_image.dart
+++ evolve_store/lib/widgets/circle_product_image.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
@@ -5,38 +6,38 @@ class CircleProductImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    return Stack(
-      children: [
-        Positioned(
-          top: -(size.height*0.35),
-          left: -(size.width*0.11),
-          child: Neumorphic(
-            padding: EdgeInsets.only(top:size.height*0.4, left: size.width*0.25, right: size.width*0.25),
-            style: NeumorphicStyle(
-              boxShape: NeumorphicBoxShape.circle(),
-              depth: -10,
-              intensity: 0.9,
-              surfaceIntensity: 0.9,
-              shape: NeumorphicShape.concave,
-              color: Colors.grey[200],
-            ),
-
-            child: Padding(
-              padding: EdgeInsets.only(bottom: 50),
-              child: Container(
-                width: 300,
-                height: 300,
-                color: Colors.transparent,
-                child: Image.asset(
-                  "assets/images/backpack.png",
-                  height: 150,
-                  width: 200,
-                ),
-              ),
-            ),
+    return Positioned(
+      top: -(size.height * 0.35),
+      left: -(size.width * 0.21),
+      child: Neumorphic(
+        padding: EdgeInsets.only(
+          top: size.height * 0.4,
+          left: size.width * 0.35,
+          right: size.width * 0.35,
+          bottom: 0,
+        ),
+        style: NeumorphicStyle(
+          boxShape: NeumorphicBoxShape.circle(),
+          depth: -10,
+          intensity: 0.9,
+          surfaceIntensity: 0.9,
+          shape: NeumorphicShape.flat,
+          color: Color(0xffe3edf4),
+        ),
+        child: Container(
+          padding: EdgeInsets.only(bottom: 30),
+          width: size.width*0.7,
+          height: size.height*0.4,
+          color: Colors.transparent,
+          child: Image.asset(
+            "assets/images/backpack.png",
+            height: 150,
+            width: 150,
+            fit: BoxFit.contain,
+            alignment: Alignment.bottomCenter,
           ),
         ),
-      ],
+      ),
     );
   }
 }
