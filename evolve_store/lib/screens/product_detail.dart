235cb01cccc426691813761618e782569fecf9ab
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-03 17:49:41 -0700

    Processing on Product detail page

--- evolve_store/lib/screens/product_detail.dart
+++ evolve_store/lib/screens/product_detail.dart
@@ -1,22 +1,45 @@
+import 'package:evolvestore/widgets/circle_product_image.dart';
+import 'package:evolvestore/widgets/option_box.dart';
+import 'package:evolvestore/widgets/TextArea.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Stack(
+    final Size size = MediaQuery.of(context).size;
+    String testdes = "askjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksaaskjflksajfksa";
+
+
+    return MaterialApp(
+      home: Scaffold(
+        body: Stack(
           children: [
-            Neumorphic(
-              padding: EdgeInsets.all(80),
-              style: NeumorphicStyle(
-                boxShape: NeumorphicBoxShape.circle()
-              ),
+            CircleProductImage(),
+            Column(
+              children: [
+                SizedBox(
+                  width: size.width,
+                  height: 350,
+                ),
+                SizedBox(
+                  height: size.height-350,
+                  child: ListView(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    scrollDirection: Axis.vertical,
+                    children: [
+                      OptionBox(),
+                      Textarea(title: "Bag", des: testdes),
+                      Textarea(title: "Bag", des: testdes),
+                    ],
+                  ),
+                )
+              ],
             )
           ],
-        )
-      ],
+        ),
+      ),
     );
   }
 }
