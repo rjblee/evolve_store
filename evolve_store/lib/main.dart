d91409d336fe86967bf3db582ac1e45ff171c05e
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-04 23:51:29 -0700

    Get product items data from FirebaseStore

--- evolve_store/lib/main.dart
+++ evolve_store/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:evolvestore/screens/product_detail.dart';
 import 'package:evolvestore/widgets/category_slide.dart';
 import 'package:evolvestore/widgets/product_slide.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,9 +10,139 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  Widget buildProduct() {
-    return ProductSlideList();
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var productList;
+  List selectedCategoryItems = [];
+
+  @override
+  initState() {
+    getCategory();
+    print(productList);
+  }
+
+  void getCategory() {
+    var firestoreInstance = Firestore.instance;
+    firestoreInstance
+        .collection("products")
+        .getDocuments()
+        .then((querySnapshot) {
+      var list = [];
+      querySnapshot.documents.forEach((result) {
+        list.add(result.data);
+      });
+
+      setState(() {
+        productList = list;
+      });
+
+    });
+  }
+
+  void filterByCategory(String categoryType) {
+    var filteredList=[];
+    productList.forEach(
+        (item) => {
+          if(item["category"] == categoryType) {
+            filteredList.add(item)
+          }
+        }
+    );
+
+    setState(() {
+      selectedCategoryItems = filteredList;
+    });
+
+    print(filteredList);
+  }
+
+  List<Widget> makeProductList(AsyncSnapshot snapshot) {
+    return snapshot.data.documents.map<Widget>((productItem) {
+      return NeumorphicButton(
+        margin: EdgeInsets.symmetric(horizontal: 15),
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 0,
+          ),
+          child: Column(
+            children: [
+              Image.asset(
+                "assets/images/backpack.png",
+                height: 260,
+              ),
+              SizedBox(height: 14),
+              Text(
+                productItem["name"],
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.left,
+              ),
+              Text(
+                "\$" + productItem["price"].toString(),
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(productItem["category"]),
+              Neumorphic(
+                padding: EdgeInsets.all(3),
+                style: NeumorphicStyle(
+                  color: Color(0xFF25d8bf),
+                  shape: NeumorphicShape.flat,
+                  depth: -10,
+                ),
+                child: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => ProductPage()));
+        },
+      );
+    }).toList();
+  }
+
+  Widget categoryItem({categoryName: String, active: false, categoryType: String}) {
+    bool isActive = active ? active : false;
+    return GestureDetector(
+      child: Container(
+        decoration: BoxDecoration(
+            color: Color(0XFFEFF3F6),
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                  color: Color.fromRGBO(0, 0, 0, 0.1),
+                  offset: Offset(6, 2),
+                  blurRadius: 6.0,
+                  spreadRadius: 3.0),
+              BoxShadow(
+                  color: Color.fromRGBO(255, 255, 255, 0.9),
+                  offset: Offset(-6, -2),
+                  blurRadius: 6.0,
+                  spreadRadius: 3.0)
+            ]),
+        margin: EdgeInsets.symmetric(horizontal: 10),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+        child: Text(
+          categoryName,
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: isActive ? Color(0xFF25d8bf) : Colors.grey),
+        ),
+      ),
+      onTap: () {
+        filterByCategory(categoryType);
+      },
+    );
   }
 
   @override
@@ -55,8 +186,40 @@ class MyApp extends StatelessWidget {
           ),
           body: Column(
             children: [
-              CategorySlideList(),
-              buildProduct(),
+              SizedBox(
+                height: 80,
+                child: ListView(
+                  padding: EdgeInsets.all(20),
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    categoryItem(categoryName: "All", active: true),
+                    categoryItem(categoryName: "Apparel", categoryType: "apparel"),
+                    categoryItem(categoryName: "Electronics",categoryType: "electronics"),
+                    categoryItem(categoryName: "Accessories",categoryType: "accessories"),
+                    categoryItem(categoryName: "Gift Cards", categoryType: "giftcard"),
+                  ],
+                ),
+              ),
+              StreamBuilder(
+                stream: Firestore.instance.collection('products').snapshots(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    default:
+                      return SizedBox(
+                        height: 400,
+                        child: ListView(
+                          padding: EdgeInsets.all(10),
+                          scrollDirection: Axis.horizontal,
+                          children: makeProductList(snapshot),
+                        ),
+                      );
+                  }
+                },
+              ),
             ],
           ),
           bottomNavigationBar: Neumorphic(
