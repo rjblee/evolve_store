45d33cadc25a265290c328f02b204c7bd05f90ba
Ralph Lee <ralph.lee@live.ca>
2020-07-04 19:34:19 -0700

    Edit size of each list and add search icon

--- evolve_store/lib/widgets/product_slide.dart
+++ evolve_store/lib/widgets/product_slide.dart
@@ -14,27 +14,43 @@ class _ProductSlideListState extends State<ProductSlideList> {
     List<Widget> makeProductList(AsyncSnapshot snapshot) {
       return snapshot.data.documents.map<Widget>((document) {
         return NeumorphicButton(
-          margin: EdgeInsets.symmetric(horizontal: 20),
+          margin: EdgeInsets.symmetric(horizontal: 15),
           child: Container(
             padding: EdgeInsets.symmetric(
-              horizontal: 10,
+              horizontal: 0,
             ),
             child: Column(
               children: [
                 Image.asset(
                   "assets/images/backpack.png",
-                  height: 150,
+                  height: 260,
                 ),
-                SizedBox(height: 16),
+                SizedBox(height: 14),
                 Text(
                   document["name"],
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.left,
                 ),
                 Text(
                   "\$" + document["price"].toString(),
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
                 Text(document["category"]),
+                Neumorphic(
+                  padding: EdgeInsets.all(3),
+                  style: NeumorphicStyle(
+                    color: Color(0xFF25d8bf),
+                    shape: NeumorphicShape.flat,
+                    depth: -10,
+                  ),
+                  child: Icon(
+                    Icons.search,
+                    color: Colors.white,
+                  ),
+                ),
               ],
             ),
           ),
@@ -56,9 +72,9 @@ class _ProductSlideListState extends State<ProductSlideList> {
             );
           default:
             return SizedBox(
-              height: 300,
+              height: 400,
               child: ListView(
-                padding: EdgeInsets.all(20),
+                padding: EdgeInsets.all(10),
                 scrollDirection: Axis.horizontal,
                 children: makeProductList(snapshot),
               ),
