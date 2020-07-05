7cf3251af13a1db488bf88848e0202f022123d8e
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-04 21:53:25 -0700

    Refactor category list

--- evolve_store/lib/widgets/category_slide.dart
+++ evolve_store/lib/widgets/category_slide.dart
@@ -12,53 +12,52 @@ class CategorySlideList extends StatefulWidget {
 class _CategorySlideListState extends State<CategorySlideList> {
   @override
   Widget build(BuildContext context) {
+
+    Widget categoryItem({categoryName: String, active : false}) {
+      bool isActive = active ? active: false;
+      return GestureDetector(
+        child: Container(
+          decoration: BoxDecoration(
+              color: Color(0XFFEFF3F6),
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                    color: Color.fromRGBO(0, 0, 0, 0.1),
+                    offset: Offset(6, 2),
+                    blurRadius: 6.0,
+                    spreadRadius: 3.0),
+                BoxShadow(
+                    color: Color.fromRGBO(255, 255, 255, 0.9),
+                    offset: Offset(-6, -2),
+                    blurRadius: 6.0,
+                    spreadRadius: 3.0)
+              ]),
+          margin: EdgeInsets.symmetric(horizontal: 10),
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+          child: Text(
+            categoryName,
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: isActive? Color(0xFF25d8bf) : Colors.grey),
+          ),
+        ),
+        onTap: () {
+          print(categoryName);
+        },
+      );
+    }
+
+
     return SizedBox(
       height: 80,
       child: ListView(
         padding: EdgeInsets.all(20),
         scrollDirection: Axis.horizontal,
         children: [
-          NeumorphicButton(
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            child: Text(
-              "All",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold, color: Color(0xFF25d8bf)),
-            ),
-            onPressed: () {},
-          ),
-          NeumorphicButton(
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            child: Text(
-              "Apparel",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            onPressed: () {},
-          ),
-          NeumorphicButton(
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            child: Text(
-              "Electronics",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            onPressed: () {},
-          ),
-          NeumorphicButton(
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            child: Text(
-              "Accessories",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            onPressed: () {},
-          ),
-          NeumorphicButton(
-            margin: EdgeInsets.symmetric(horizontal: 10),
-            child: Text(
-              "Gift Cards",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            onPressed: () {},
-          ),
+          categoryItem(categoryName: "All", active: true),
+          categoryItem(categoryName: "Apparel"),
+          categoryItem(categoryName: "Electronics"),
+          categoryItem(categoryName: "Accessories"),
+          categoryItem(categoryName: "Gift Cards"),
         ],
       ),
     );
