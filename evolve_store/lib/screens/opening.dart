335726bae210af6b01d5028cd6130326411b5234
Ralph Lee <ralph.lee@live.ca>
2020-07-06 16:31:01 -0700

    Set up the opening page of the app

---
+++ evolve_store/lib/screens/opening.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+class OpeningPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black45,
+      child: Neumorphic(
+//        child: NeumorphicButton(
+//          "Enter",
+//          onPressed: () {
+//            Navigator.push(
+//              context,
+//              MaterialPageRoute(
+//                            builder: (context) => (),
+//                  ),
+//            );
+//          },
+//        ),
+          ),
+    );
+  }
+}
