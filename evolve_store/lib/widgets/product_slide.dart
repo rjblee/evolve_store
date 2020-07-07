9ad33e0c924d70ddeccd52a16c7df87affcf38fd
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-07 01:48:33 -0700

    Take out ProductList from main.dart

--- evolve_store/lib/widgets/product_slide.dart
+++ evolve_store/lib/widgets/product_slide.dart
@@ -1,86 +1,87 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:evolvestore/screens/product_detail.dart';
+import 'package:evolvestore/widgets/custom_neumorphic.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
-class ProductSlideList extends StatefulWidget {
-  @override
-  _ProductSlideListState createState() => _ProductSlideListState();
-}
+class ProductList extends StatelessWidget {
+  List selectedCategoryItems;
 
-class _ProductSlideListState extends State<ProductSlideList> {
+  ProductList({this.selectedCategoryItems});
+
+  Widget makeProductList(productItem, context) {
+    final Size size = MediaQuery.of(context).size;
+
+    return GestureDetector(
+      child: CustomNeumorphic(
+        margin: EdgeInsets.symmetric(horizontal: 15),
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 0,
+            vertical: 15,
+          ),
+          child: Column(
+            children: [
+              Image.network(
+                productItem["image"],
+                height: size.height * 0.3,
+                width: size.width * 0.6,
+              ),
+              SizedBox(height: 14),
+              Text(
+                productItem["name"],
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.left,
+              ),
+              Text(
+                "\$" + productItem["price"].toString(),
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(productItem["category"]),
+              Neumorphic(
+                padding: EdgeInsets.all(3),
+                style: NeumorphicStyle(
+                  color: Color(0xFF25d8bf),
+                  shape: NeumorphicShape.flat,
+                  depth: -10,
+                ),
+                child: Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ProductPage(productDetail: productItem),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    List<Widget> makeProductList(AsyncSnapshot snapshot) {
-      return snapshot.data.documents.map<Widget>((document) {
-        return NeumorphicButton(
-          margin: EdgeInsets.symmetric(horizontal: 15),
-          child: Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 0,
-            ),
-            child: Column(
-              children: [
-                Image.asset(
-                  "assets/images/backpack.png",
-                  height: 260,
-                ),
-                SizedBox(height: 14),
-                Text(
-                  document["name"],
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.left,
-                ),
-                Text(
-                  "\$" + document["price"].toString(),
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text(document["category"]),
-                Neumorphic(
-                  padding: EdgeInsets.all(3),
-                  style: NeumorphicStyle(
-                    color: Color(0xFF25d8bf),
-                    shape: NeumorphicShape.flat,
-                    depth: -10,
-                  ),
-                  child: Icon(
-                    Icons.search,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ),
-//          onPressed: () {
-//            Navigator.push(context,
-//                MaterialPageRoute(builder: (context) => ProductPage()));
-//          },
-        );
-      }).toList();
-    }
-
-    return StreamBuilder(
-      stream: Firestore.instance.collection('products').snapshots(),
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          default:
-            return SizedBox(
-              height: 400,
-              child: ListView(
-                padding: EdgeInsets.all(10),
-                scrollDirection: Axis.horizontal,
-                children: makeProductList(snapshot),
-              ),
-            );
-        }
-      },
+    final Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      height: size.height * 0.6,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: selectedCategoryItems.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            padding: EdgeInsets.symmetric(vertical: size.height * 0.05),
+            child: makeProductList(selectedCategoryItems[index], context),
+          );
+        },
+      ),
     );
   }
 }
