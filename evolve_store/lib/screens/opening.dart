bb49c65f79e0e500ed57087446a8438e1a278721
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-07 01:46:05 -0700

    Create opening-page

--- evolve_store/lib/screens/opening.dart
+++ evolve_store/lib/screens/opening.dart
@@ -1,24 +1,46 @@
+import 'package:evolvestore/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
-class OpeningPage extends StatelessWidget {
+class Opening extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.black45,
-      child: Neumorphic(
-//        child: NeumorphicButton(
-//          "Enter",
-//          onPressed: () {
-//            Navigator.push(
-//              context,
-//              MaterialPageRoute(
-//                            builder: (context) => (),
-//                  ),
-//            );
-//          },
-//        ),
+      color: Colors.black,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+              "https://media-exp1.licdn.com/dms/image/C560BAQHFG0abeBdddQ/company-logo_200_200/0?e=2159024400&v=beta&t=4ptxfrJ5ow5YC7jot-Q3ykQWeGlWLWZROgKrF8qR120"),
+          GestureDetector(
+            child: Neumorphic(
+              style: NeumorphicStyle(
+                color: Colors.grey,
+              ),
+              margin: EdgeInsets.symmetric(horizontal: 80),
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Center(
+                child: Text(
+                  'ENTER',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  )
+              );
+            },
           ),
+        ],
+      ),
     );
   }
 }
