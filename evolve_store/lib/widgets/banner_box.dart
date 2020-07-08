d277f8270673dbea5c6b2359765904dbeae15be5
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-08 04:08:57 -0700

    Create BannerBox class

---
+++ evolve_store/lib/widgets/banner_box.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+class BannerBox extends StatelessWidget {
+  List<Color> gradation;
+  String text;
+
+  BannerBox({this.gradation:const [Colors.greenAccent, Colors.green], this.text,});
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Neumorphic(
+        style: NeumorphicStyle(
+          depth: 5,
+        ),
+        child: Container(
+          height: 70,
+          margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: gradation,
+            ),
+          ),
+          child: Text(
+            text,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
