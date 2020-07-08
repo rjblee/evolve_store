33edd2a68e12bc9a964f9bfdd9154af15e190b39
Eunseon-Jang <35230852+Eunseon-Jang@users.noreply.github.com>
2020-07-07 19:19:00 -0700

    Refactor PurchaseButton set and add increase, decrease feature

--- evolve_store/lib/screens/product_detail.dart
+++ evolve_store/lib/screens/product_detail.dart
@@ -55,46 +55,82 @@ class ProductPage extends StatelessWidget {
                   Positioned(
                     bottom: 0,
                     left: 0,
-                    child: Container(
-                      height: 60,
-                      decoration: BoxDecoration(),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          GestureDetector(
-                            child: CustomNeumorphic(
-                              width: 40,
-                              height: 40,
-                              child: Icon(Icons.remove, color: Colors.grey),
-                            ),
-                            onTap: (){},
-                          ),
-                          Text("1", style: TextStyle(fontWeight: FontWeight.bold),),
-                          GestureDetector(
-                            child: CustomNeumorphic(
-                              width: 40,
-                              height: 40,
-                              child: Icon(Icons.add,color: Colors.grey,),
-                            ),
-                            onTap: (){},
-                          ),
-                          GestureDetector(
-                            child: CustomNeumorphic(
-                              width: 150,
-                              height: 40,
-                              child: Center(child: Text("View Cart  >", style: TextStyle(color: Colors.grey),)),
-                            ),
-                            onTap: (){},
-                          ),
-                        ],
-                      ),
-                    ),
+                    child: PurchaseButtonSet(),
                   )
                 ],
               )
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class PurchaseButtonSet extends StatefulWidget {
+  const PurchaseButtonSet({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _PurchaseButtonSetState createState() => _PurchaseButtonSetState();
+}
+
+class _PurchaseButtonSetState extends State<PurchaseButtonSet> {
+  int productCount = 0;
+  void increase() {
+    setState(() {
+      productCount++;
+    });
+  }
+
+  void decrease() {
+    setState(() {
+      if(productCount<=0) {
+        return;
+      }
+      productCount--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          GestureDetector(
+            child: CustomNeumorphic(
+              width: 40,
+              height: 40,
+              child: Icon(Icons.remove, color: Colors.grey),
+            ),
+            onTap: (){
+              decrease();
+            },
+          ),
+          Text(productCount.toString(), style: TextStyle(fontWeight: FontWeight.bold),),
+          GestureDetector(
+            child: CustomNeumorphic(
+              width: 40,
+              height: 40,
+              child: Icon(Icons.add,color: Colors.grey,),
+            ),
+            onTap: (){
+              increase();
+            },
+          ),
+          GestureDetector(
+            child: CustomNeumorphic(
+              width: 150,
+              height: 40,
+              child: Center(child: Text("Buy now  >", style: TextStyle(color: Colors.grey),)),
+            ),
+            onTap: (){},
+          ),
+        ],
       ),
     );
   }
